6f47ce8f84b5af303b3349ee32a046bca0922770
Bohdan Jar <bohdanjar@Bohdans-MacBook-Pro.local>
2022-04-27 20:55:33 +0200

    t

--- lib/application/application.dart
+++ lib/application/application.dart
@@ -4,6 +4,8 @@ import 'package:ui_playground/ui/weather_app/home/weather_home_page.dart';
 class Application extends StatelessWidget {
   const Application({Key? key}) : super(key: key);
 
+  //Todo (Bohdan): Navigator 2.0
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
